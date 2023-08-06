7053cab8585762281a658ad0ab350a9fa5cb3d8a
Tomasz Bawor <bawortomasz@gmail.com>
2023-08-06 19:52:42 +0200

    Code structure fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use crate::components::App::App;
 use leptos::*;
 
+mod components;
 mod error;
 mod prelude;
 
@@ -9,20 +11,4 @@ fn main() {
             cx, <App/>
         }
     })
-}
-
-#[component]
-fn App(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
-
-    view! { cx,
-        <button
-            on:click=move |_| {
-                set_count.set(count.get()   + 1);
-            }
-        >
-            "Click me: "
-            {move || count.get()}
-        </button>
-    }
 }
