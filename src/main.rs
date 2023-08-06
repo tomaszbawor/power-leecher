c5af605d17090877e56557320d9765d0caf53121
Tomasz Bawor <bawortomasz@gmail.com>
2023-08-06 18:53:56 +0200

    Add component

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use leptos::*;
+
 mod error;
 mod prelude;
 
 fn main() {
     mount_to_body(|cx| {
         view! {
-            cx, <p>"Hello, world!"</p>
+            cx, <App/>
         }
     })
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.set(count.get()   + 1);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
 }
